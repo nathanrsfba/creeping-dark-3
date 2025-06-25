229540067e4a83f4140466d34eb5acf5877d4ac3
Nathan Roberts <nathanr@tardislabs.com>
2025-06-24 23:44:07 -0700

    Added further machine recipes for certus ore

--- kubejs/server_scripts/certus_processing.js
+++ kubejs/server_scripts/certus_processing.js
@@ -2,8 +2,10 @@
 
 ServerEvents.recipes( event => {
 
-    // Add smelting recipe
+    // Add smelting and blasting recipes
     event.smelting( 'ae2:certus_quartz_crystal', 
+        'kubejs:certus_ore' ).xp( 0.2 );
+    event.blasting( 'ae2:certus_quartz_crystal', 
         'kubejs:certus_ore' ).xp( 0.2 );
 
     // Add pulverizer recipe
@@ -13,9 +15,82 @@ ServerEvents.recipes( event => {
         "result": [
             {
                 "item": "ae2:certus_quartz_crystal",
-                "count": 4
+                "count": 2
+            },
+            {
+                "item": "ae2:certus_quartz_dust",
+                "chance": 0.1
             }
         ]
     });
+
+    // Add Enrichment Chamber recipe
+    event.custom({
+        "type": "mekanism:enriching",
+        "input": {"ingredient": {"tag": "forge:ores/certus_quartz" }},
+        "output": {"count":4, "item":"ae2:certus_quartz_crystal"}
+    });
+
+    // Add Crushing Wheel recipe
+    event.custom({
+        "type": "create:crushing",
+        "ingredients": [
+            {"tag": "forge:ores/certus_quartz" }
+        ],
+        "processingTime": 250,
+        "results": [
+            {
+                "count": 2,
+                "item": "ae2:certus_quartz_crystal"
+            },
+            {
+                "chance": 0.1,
+                "item": "ae2:certus_quartz_dust",
+            },
+            {
+                "chance": 0.2,
+                "item": "create:experience_nugget"
+            }
+        ]
+    });
+
+    // Add SAG Mill Recipe
+    event.custom({
+        "type": "enderio:sag_milling",
+        "energy": 2400,
+        "input": {"tag": "forge:ores/certus_quartz"},
+        "outputs": [
+            {
+                "chance": 1.0,
+                "item": {
+                    "count": 2,
+                    "item": "ae2:certus_quartz_crystal"
+                },
+                "optional": false
+            },
+            {
+                "chance": 0.1,
+                "item": {
+                    "item": "ae2:certus_quartz_dust",
+                },
+                "optional": false
+            }
+        ]
+    });
+
+    // Add IE Crusher Recipe
+    event.custom({
+        "energy" : 6000,
+        "input": {"tag": "forge:ores/certus_quartz"},
+        "result" : {
+            "base_ingredient" : {
+                "item": "ae2:certus_quartz_crystal"
+            },
+            "count" : 2
+        },
+        "secondaries" : [],
+        "type" : "immersiveengineering:crusher"
+    });
+        
 
 })
