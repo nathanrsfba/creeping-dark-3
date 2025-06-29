f51639bd9faa91427ed93504692a32d9b13c4dc5
Nathan Roberts <nathanr@tardislabs.com>
2025-06-29 01:06:00 -0700

    Created a custom texture for the aether portal

--- kubejs/startup_scripts/aetherportal.js
+++ kubejs/startup_scripts/aetherportal.js
@@ -10,7 +10,4 @@ StartupEvents.registry( 'block', event => {
         .resistance( 1.0 )
         .lightLevel( 1.0 )
         .requiresTool( true )
-        .textureAll( "minecraft:block/glowstone" )
-        // This method is totally undocumented, so this is just a guess
-        .color( 0, "#ff80ff" )
 })
