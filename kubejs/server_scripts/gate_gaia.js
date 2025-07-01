e0bdace210caa95db91e26527d2a237d83c9601e
Nathan Roberts <nathanr@tardislabs.com>
2025-07-01 16:36:45 -0700

    Gated Gaia to Thermal Expansion crystallizer
    Change of plans. Thermal is going to unlock Gaia, and RFTools is going
    to unlock Solara

---
+++ kubejs/server_scripts/gate_gaia.js
@@ -0,0 +1,29 @@
+// priority: 0
+
+ServerEvents.recipes( event => {
+    event.remove({ output: 'gaiadimension:crystallized_lapis_lazuli' });
+    event.remove({ output: 'gaiadimension:crystallized_redstone' });
+    event.custom({
+        type: "thermal:crystallizer",
+        ingredients: [
+            { fluid: "thermal:redstone", amount: 100 },
+            { item: "minecraft:redstone" }
+        ],
+        result: [
+            { item: "gaiadimension:crystallized_redstone" }
+        ]
+    });
+    event.custom({
+        type: "thermal:crystallizer",
+        ingredients: [
+            { fluid: "minecraft:water", amount: 100 },
+            { item: "minecraft:lapis_lazuli" },
+            { item: "thermal:apatite_dust" },
+        ],
+        result: [
+            { item: "gaiadimension:crystallized_lapis_lazuli" }
+    
+        ]
+    });
+
+});
