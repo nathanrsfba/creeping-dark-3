9dc1aafa22f111bc8e3c0c69d3ab451f07c2e179
Nathan Roberts <nathanr@tardislabs.com>
2025-07-03 02:06:20 -0700

    Started adding some of the custom key items

---
+++ kubejs/startup_scripts/endkey.js
@@ -0,0 +1,23 @@
+// priority: 0
+
+// Add End Key
+StartupEvents.registry( 'item', event => {
+    // Add the end key
+    event.create( 'end_orb' ).displayName( 'End Orb' )
+
+    // Minibosses/dungeons loot:
+    // Horned tower: ? (unsure of either dimension or miniboss)
+    // Space Needle: ? (Found on glacio, unsure of miniboss)
+    // Broken Sphere: Tropics, Eye of head (no custom loot needed)
+    // Underground Lab: Probably Solara, unsure of miniboss
+    // Spider Dome: Mining Dimension, evil eye
+    // Void City: Dimensional storage, unsure of loot
+
+    // Spider loot
+    event.create( 'evil_eye' ).displayName( 'Evil Eye' );
+    // Lab loot
+    event.create( 'quartz_bone' ).displayName( 'Quartz Bone' );
+    // Not sure what this will be for, but let's
+    // yoink it from CD2 on general principles
+    event.create( 'shadow_flesh' ).displayName( 'Shadow Flesh' );
+})
