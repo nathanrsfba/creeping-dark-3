4fa59658483b55fbef8f31a985a06b60f3f0c1b3
Nathan Roberts <nathanr@tardislabs.com>
2025-07-03 13:47:38 -0700

    Began adding end orb

---
+++ kubejs/server_scripts/endkey.js
@@ -0,0 +1,93 @@
+// priority: 0
+
+ServerEvents.tags( 'item', event => {
+
+    // Tag Valkyrie loot
+    [
+        "aether:valkyrie_helmet",
+        "aether:valkyrie_chestplate",
+        "aether:valkyrie_leggings",
+        "aether:valkyrie_boots",
+        "aether:valkyrie_gloves",
+        "aether:valkyrie_pickaxe",
+        "aether:valkyrie_axe",
+        "aether:valkyrie_shovel",
+        "aether:valkyrie_hoe",
+        "aether:lightning_sword",
+        "aether:holy_sword",
+        "aether:invisibility_cloak",
+        "aether:regeneration_stone",
+        "aether:music_disc_ascending_dawn",
+        "aether:valkyrie_cape"
+    ].forEach( item => {
+        event.add( 'kubejs:valkyrie_loot', item );
+    });
+
+    // Tag slider loot
+    [
+        "aether:agility_cape",
+        "aether:cloud_staff",
+        "aether:flaming_sword",
+        "aether:hammer_of_kingbdogz",
+        "aether:music_disc_sliders_wrath",
+        "aether:neptune_boots",
+        "aether:neptune_chestplate",
+        "aether:neptune_gloves",
+        "aether:neptune_helmet",
+        "aether:neptune_leggings",
+        "aether:phoenix_bow",
+        "aether:sentry_boots",
+        "aether:shield_of_repulsion",
+        "aether:swet_cape",
+        "aether:valkyrie_lance"
+    ].forEach( item => {
+        event.add( 'kubejs:slider_loot', item );
+    });
+
+    // Tag Malachite Guard loot
+    [
+        "gaiadimension:malachite_guard_baton",
+        "gaiadimension:malachite_guard_headgear",
+        "gaiadimension:malachite_guard_brace",
+        "gaiadimension:malachite_guard_gear",
+        "gaiadimension:malachite_guard_boots"
+    ].forEach( item => {
+        event.add( 'kubejs:malachite_loot', item );
+    });
+
+
+
+});
+
+ServerEvents.recipes( event => {
+    event.custom({
+        "type": "create:mechanical_crafting",
+        "acceptMirrored": false,
+        "result": { "item": "kubejs:end_orb" },
+        "key": {
+            "A": { "item": "blue_skies:poison_arc" }, // Arachnarch drop  
+            // "C": { "tag": "" }, // Create item  
+            "F": { "item": "undergarden:forgotten_nugget" },
+            "M": { "tag": "kubejs:malachite_loot" }, // Malachite Guard loot  
+            "N": { "item": "blue_skies:nature_arc" },
+            "S": { "tag": "kubejs:slider_loot" },
+            "V": { "tag": "kubejs:valkyrie_loot" },
+            // "W": { "item": "" }, // Warden drop  
+            "a": { "item": "astral_dimension:astral_amulet" },
+            "e": { "item": "tropicraft:eih_spawn_egg" }, // Eye of head  
+            // "h": { "item": "" }, // Horned tower loot  
+            "n": { "item": "minecraft:nether_star" },
+            "q": { "item": "kubejs:quartz_bone" }, // Quartz bone  
+            // "s": { "item": "" }, // Space Needle drop  
+            // "v": { "item": "" }, // Void city item  
+            "w": { "item": "ars_nouveau:wilden_tribute" }  // Wilden tribute  
+        },
+        "pattern": [
+            "  q  ",
+            " VFS ",
+            " ean ",
+            " AMN ",
+            "  w  "
+        ]
+    });
+});
