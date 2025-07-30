bd4944536596b966fd8f095cd93ba9da9568f3e2
Nathan Roberts <nathanr@tardislabs.com>
2025-07-30 14:56:41 -0700

    Gate Setback to Ars
    Uses the enchantment apparatus to craft the nanta star

---
+++ kubejs/server_scripts/gate_sb.js
@@ -0,0 +1,22 @@
+// priority: 0
+
+// Gate Setback to Ars
+
+ServerEvents.recipes( event => {
+    event.remove({ output: "astral_dimension:setback" });
+    event.custom({
+        type: "ars_nouveau:enchanting_apparatus",
+        keepNbtOfReagent: false,
+        output: { item: "astral_dimension:setback" },
+        pedestalItems: [
+            { item: "astral_dimension:nanta_pearl" },
+            { item: "astral_dimension:nanta_pearl" },
+            { item: "astral_dimension:nanta_pearl" },
+            { item: "astral_dimension:nanta_pearl" }
+        ],
+        reagent: [
+            { item: "astral_dimension:faded_star" }
+        ],
+        sourceCost: 1000
+    });
+});
