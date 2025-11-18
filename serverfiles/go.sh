05c343bb45573ce3939a31758d06795b0a03e9c8
Nathan Roberts <nathanr@tardislabs.com>
2025-11-17 18:00:12 -0800

    Fixed a hardcoded path in go.sh

--- serverfiles/go.sh
+++ serverfiles/go.sh
@@ -48,7 +48,7 @@ echo "Starting server..."
 # Add custom JVM arguments to the user_jvm_args.txt
 # Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
 #  pass them to this script directly
-"$JAVAEXE" @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.4.0/unix_args.txt "$@"
+"$JAVAEXE" @user_jvm_args.txt @libraries/net/minecraftforge/forge/$MCVER-$FORGEVER/unix_args.txt "$@"
 
 echo ""
 echo "Server exited. To restart, run:"
